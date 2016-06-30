a46c9172683dd975f19b9790cbc97a7b695144e7
Mariusz Serwin <mariusz.serwin@praemium.com>
2016-06-30 09:39:45 +0100

    testing modules

---
+++ Modules/get_host/get_host.ps1
@@ -0,0 +1,9 @@
+﻿#!powershell
+# WANT_JSON
+# POWERSHELL_COMMON
+$data = Get-Host | Select Version
+$result = New-Object psobject @{
+get_host_version = $data
+changed = $false
+};
+Exit-Json $result;
---
+++ Modules/get_host/get_host.py
@@ -0,0 +1,14 @@
+﻿DOCUMENTATION = '''
+---
+module: get_host
+version_added: "0.1"
+short_description: Call Get-Host cmdlet.
+description:
+- Call Get-Host cmdlet
+'''
+EXAMPLES = '''
+# Test connectivity to a windows host
+ansible winserver -m get_host
+# Example from an Ansible Playbook
+- action: get_host
+'''
